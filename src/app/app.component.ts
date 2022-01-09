6da831293936f9b5fc4b293146eca3d901fe0870
Juhiisingh <juhisingh99844@gmail.com>
2022-01-09 16:45:33 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bus_Booking';
-}
+  constructor()
+  {
+    setTimeout(()=>{
+      this.title = "Changed Title";},2000);
+    }
+  }
+
